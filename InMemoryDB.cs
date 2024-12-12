25e95b2ced5eab4e1144b965a1a002d197d2cc41
devdrum <bdrumgool@gmail.com>
2024-12-11 21:00:32 -0500

    Initial commit

---
+++ InMemoryDB.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class InMemoryDB
+{
+    private Dictionary<string, int> _current;
+    private Dictionary<string, int> _pending;
+    private bool _transactionInProgress = false;
+
+
+
+
+    public InMemoryDB()
+    {
+        _current = [];
+        _pending = [];
+    }
+
+
+    public int? Get(string key)
+    {
+        if (_current.TryGetValue(key, out int value))
+        {
+            Console.WriteLine(value.ToString());
+            return value;
+        }
+        else
+        {
+            Console.WriteLine("NULL");
+            return null;
+        }
+    }
+    public void Put(string key, int value)
+    {
+        if (_transactionInProgress)
+        {
+            _pending[key] = value;
+        }
+        else
+        {
+            Console.WriteLine("ERROR: A transaction is not in progress");
+        }
+    }
+    public void Begin_Transaction()
+    {
+        if (_transactionInProgress)
+        {
+            Console.WriteLine("ERROR: A transaction is already in progress");
+        }
+        else
+        {
+            _transactionInProgress = true;
+            _pending = new Dictionary<string, int>(_current);
+        }
+    }
+    public void Commit()
+    {
+        if (!_transactionInProgress)
+        {
+            Console.WriteLine("ERROR: A transaction is not in progress");
+        }
+        else
+        {
+            _current = new Dictionary<string, int>(_pending);
+            _transactionInProgress = false;
+        }
+    }
+    public void Rollback()
+    {
+        if (!_transactionInProgress)
+        {
+            Console.WriteLine("ERROR: A transaction is not in progress");
+        }
+        else
+        {
+            _pending = new Dictionary<string, int>(_current);
+            _transactionInProgress = false;
+        }
+    }
+
+
+}
+
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+InMemoryDB inmemoryDB = new();
+
+// should return null, because A doesn’t exist in the DB yet
+inmemoryDB.Get("A");
+
+// should throw an error because a transaction is not in progress
+inmemoryDB.Put("A", 5);
+
+// starts a new transaction
+inmemoryDB.Begin_Transaction();
+
+// set’s value of A to 5, but its not committed yet
+inmemoryDB.Put("A", 5);
+
+// should return null, because updates to A are not committed yet
+inmemoryDB.Get("A");
+
+// update A’s value to 6 within the transaction
+inmemoryDB.Put("A", 6);
+
+// commits the open transaction
+inmemoryDB.Commit();
+
+// should return 6, that was the last value of A to be committed
+inmemoryDB.Get("A");
+
+// throws an error, because there is no open transaction
+inmemoryDB.Commit();
+
+// throws an error because there is no ongoing transaction
+inmemoryDB.Rollback();
+
+// should return null because B does not exist in the database
+inmemoryDB.Get("B");
+
+// starts a new transaction
+inmemoryDB.Begin_Transaction();
+
+// Set key B’s value to 10 within the transaction
+inmemoryDB.Put("B", 10);
+
+// Rollback the transaction - revert any changes made to B
+inmemoryDB.Rollback();
+
+// Should return null because changes to B were rolled back
+inmemoryDB.Get("B");
+
